0f568182f62c19173966760a45f1a62e0a8d399f
RWCoder <dsthomson314@gmail.com>
2025-04-29 21:46:46 -0400

    Add left control mod layer

--- keyboards/keychron/q11/ansi_encoder/keymaps/my_cdh_layout/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/my_cdh_layout/keymap.c
@@ -3,6 +3,7 @@
 #define W(kc) LGUI(kc)
 #define TAP(kc) tap_code16(kc)
 #define DELAY(ms) SEND_STRING(SS_DELAY(ms))
+#define L_CTRL LM(LEFT_CTRL, MOD_LCTL)
 
 /**
  * 17
@@ -15,9 +16,10 @@
 
 enum layers {
     BASE,
-    CONTROL,
+    FUNCTION,
     SYMBOL,
     CODE,
+    LEFT_CTRL,
     QWERTY,
     BOARD
 };
@@ -37,15 +39,15 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_91_ansi(
-        C(KC_0),  KC_ESC,   W(KC_1),       W(KC_2),  W(KC_3),    W(KC_4),      W(KC_5),  W(KC_6),  W(KC_7),  W(KC_8),  W(KC_9),         W(KC_0),     KC_F11,   KC_F12,   C(A(KC_DEL)),  KC_PSCR, KC_MUTE,
-        _______,  KC_GRV,   KC_1,          KC_2,     KC_3,       KC_4,         KC_5,     KC_6,     KC_7,     KC_8,     KC_9,            KC_0,        KC_MINS,  KC_EQL,   KC_BSPC,       KC_DEL,
-        ZOOM,     KC_TAB,   KC_Q,          KC_W,     KC_F,       KC_P,         KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,            KC_SLSH,     KC_LBRC,  KC_RBRC,  KC_F9,         KC_HOME,
-        _______,  KC_ENT,   KC_A,          KC_R,     KC_S,       KC_T,         KC_G,     KC_K,     KC_N,     KC_E,     KC_I,            KC_O,        KC_QUOT,  KC_ENT,   KC_END,
-        _______,  KC_LCTL,  LSFT_T(KC_X),  KC_C,     KC_D,       KC_V,         KC_Z,     KC_H,     KC_M,     KC_COMM,  LSFT_T(KC_DOT),  MO(SYMBOL),  CW_TOGG,  KC_F12,
-        _______,  KC_LCTL,  KC_LGUI,       KC_LALT,  A(KC_SPC),  MO(CONTROL),  KC_SPC,   KC_RGUI,  KC_RALT,  KC_RCTL,  KC_F8,           KC_F11,      KC_F10
+        C(KC_0),  KC_ESC,   W(KC_1),       W(KC_2),  W(KC_3),    W(KC_4),       W(KC_5),  W(KC_6),  W(KC_7),  W(KC_8),  W(KC_9),         W(KC_0),     KC_F11,   KC_F12,   C(A(KC_DEL)),  KC_PSCR, KC_MUTE,
+        _______,  KC_GRV,   KC_1,          KC_2,     KC_3,       KC_4,          KC_5,     KC_6,     KC_7,     KC_8,     KC_9,            KC_0,        KC_MINS,  KC_EQL,   KC_BSPC,       KC_DEL,
+        ZOOM,     KC_TAB,   KC_Q,          KC_W,     KC_F,       KC_P,          KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,            KC_SLSH,     KC_LBRC,  KC_RBRC,  KC_F9,         KC_HOME,
+        _______,  KC_ENT,   KC_A,          KC_R,     KC_S,       KC_T,          KC_G,     KC_K,     KC_N,     KC_E,     KC_I,            KC_O,        KC_QUOT,  KC_ENT,   KC_END,
+        _______,  KC_LCTL,  LSFT_T(KC_X),  KC_C,     KC_D,       KC_V,          KC_Z,     KC_H,     KC_M,     KC_COMM,  LSFT_T(KC_DOT),  MO(SYMBOL),  CW_TOGG,  KC_F12,
+        _______,  L_CTRL,   KC_LGUI,       KC_LALT,  A(KC_SPC),  MO(FUNCTION),  KC_SPC,   KC_RGUI,  KC_RALT,  KC_RCTL,  KC_F8,           KC_F11,      KC_F10
     ),
 
-    [CONTROL] = LAYOUT_91_ansi(
+    [FUNCTION] = LAYOUT_91_ansi(
         _______,  _______,     _______,   _______,     _______,     _______,        _______,  _______,  _______,  _______,  _______,     _______,     _______,  _______,    _______,  _______,  TO(BOARD),
         _______,  VDI_X_HOME,  KC_F1,     KC_F2,       KC_F3,       KC_F4,          KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,      KC_F11,   KC_F12,     _______,  KC_INS,
         _______,  W(KC_LEFT),  WIN_SNAP,  WWW_POPOUT,  W(KC_UP),    C(KC_L),        MS_BTN4,  MS_BTN5,  KC_UP,    ALT_TAB,  CTRL_TAB,    C(KC_BSPC),  _______,  _______,    _______,  KC_PGUP,
@@ -70,6 +72,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  C(A(KC_DOT)),  KC_F24,   KC_F22,   A(KC_S),  KC_F20,   _______,  _______,  KC_F13,   KC_F17,   KC_F15,   _______,  _______,  _______,  _______,
         _______,  _______,       _______,  _______,  KC_F14,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+    ),
+    
+    [LEFT_CTRL] = LAYOUT_91_ansi(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  KC_LSFT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_DOT,   _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
     [QWERTY] = LAYOUT_91_ansi(
@@ -178,11 +189,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [BASE]    = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [CONTROL] = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [SYMBOL]  = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [CODE]    = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [QWERTY]  = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [BOARD]   = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(RM_PREV, RM_NEXT) }
+    [BASE]      = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [FUNCTION]  = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [SYMBOL]    = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [CODE]      = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [LEFT_CTRL] = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [QWERTY]    = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [BOARD]     = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(RM_PREV, RM_NEXT) }
 };
 #endif // ENCODER_MAP_ENABLE
